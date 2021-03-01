903b079754108f7f420d32fe99ce818e5bd39af9
Mohamed L'habib KARMOUS <69728742+mohamedkarmous@users.noreply.github.com>
2021-03-01 17:39:06 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing import image
+image_path="covid2.jpg"
+"""
+model = tf.keras.models.load_model('my_model')
+
+# Check its architecture
+model.summary()
+
+img = image.load_img(image_path, target_size=(256, 256))
+img = image.img_to_array(img)
+img = np.expand_dims(img, axis=0)
+p = model.predict(img)
+classes=["COVID","NORMAL"]
+print(classes[int(p[0][0])])
+"""
+
+class _CovidClassifier:
+    model= None
+    _mapping = [
+        "COVID",
+        "NORMAL"
+    ]
+    _instance = None
+
+    def predict(self,image_path):
+        img = image.load_img(image_path, target_size=(256, 256))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        p = self.model.predict(img)
+        classes = ["COVID", "NORMAL"]
+        return (self._mapping[int(p[0][0])])
+
+def CovidClassifier():
+    if _CovidClassifier._instance is None:
+        _CovidClassifier._instance = _CovidClassifier()
+        _CovidClassifier.model = tf.keras.models.load_model('my_model')
+    return _CovidClassifier._instance
---
+++ server.py
@@ -0,0 +1,60 @@
+import random
+from flask import Flask, request, jsonify,render_template,redirect
+from main import CovidClassifier
+import os, shutil
+
+
+
+
+
+app = Flask(__name__,static_url_path="/static")
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    image = request.files["file"]
+    file_name = str(random.randint(0, 100000)) + ".jpg"
+    image.save(file_name)
+    model = CovidClassifier()
+    result= model.predict(file_name)
+    print(result)
+    os.remove(file_name)
+    result = {"result": result}
+
+    running= False
+
+    return jsonify(result)
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+
+    result = " "
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            image = request.files["file"]
+            file_name = str(random.randint(0, 100000)) + ".jpg"
+            image.save(file_name)
+            model = CovidClassifier()
+            result = model.predict(file_name)
+            print(result)
+            os.remove(file_name)
+            result = {"result": result}
+            result=result['result']
+
+
+
+    return render_template('index.html', result=result)
+
+
+if __name__ == "__main__":
+
+    app.run(debug=False,threaded=True)
