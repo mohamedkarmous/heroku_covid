8f33ac70d0b287d44b769451efa381f03d633fbc
mohamedkarmous <mohamedlahbibkarmous@gmail.com>
2021-03-24 23:08:49 +0100

    24/03/2021

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-image_path="covid2.jpg"
+image_path = "covid2.jpg"
 """
 model = tf.keras.models.load_model('my_model2.hdf5')
 
@@ -19,15 +19,16 @@ classes=["COVID","NORMAL"]
 print(classes[int(p[0][0])])
 """
 
+
 class _CovidClassifier:
-    model= None
+    model = None
     _mapping = [
         "COVID",
         "NORMAL"
     ]
     _instance = None
 
-    def predict(self,image_path):
+    def predict(self, image_path):
         img = image.load_img(image_path, target_size=(256, 256))
         img = image.img_to_array(img)
         img = np.expand_dims(img, axis=0)
@@ -35,8 +36,9 @@ class _CovidClassifier:
         classes = ["COVID", "NORMAL"]
         return (self._mapping[int(p[0][0])])
 
+
 def CovidClassifier():
     if _CovidClassifier._instance is None:
         _CovidClassifier._instance = _CovidClassifier()
-        _CovidClassifier.model = tf.keras.models.load_model('my_model2.h5')
+        _CovidClassifier.model = tf.keras.models.load_model('my_model2.hdf5')
     return _CovidClassifier._instance
