38adf8aa72476fb044d202f1d00c09743c2517de
mohamedkarmous <mohamedlahbibkarmous@gmail.com>
2021-04-17 15:36:25 +0100

    added keras to requ

--- main.py
+++ main.py
@@ -2,23 +2,10 @@ import os
 
 import numpy as np
 
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
+from keras.preprocessing import image
+from keras.models import Model
 
-"""
-model = tf.keras.models.load_model('my_model2.hdf5')
 
-# Check its architecture
-model.summary()
-
-img = image.load_img(image_path, target_size=(256, 256))
-img = image.img_to_array(img)
-img = np.expand_dims(img, axis=0)
-p = model.predict(img)
-classes=["COVID","NORMAL"]
-print(classes[int(p[0][0])])
-"""
 
 
 def get_proba(l):
@@ -56,6 +43,6 @@ class _CovidClassifier:
 def CovidClassifier():
     if _CovidClassifier._instance is None:
         _CovidClassifier._instance = _CovidClassifier()
-        _CovidClassifier.model = tf.keras.models.load_model('my_model_3.h5')
+        _CovidClassifier.model = keras.models.load_model('my_model_3.h5')
         _CovidClassifier.intermediate_layer_model = Model(inputs=_CovidClassifier.model.input,outputs=_CovidClassifier.model.get_layer("dense_19").output)
     return _CovidClassifier._instance
